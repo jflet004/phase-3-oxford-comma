3f9a47e61e2b6010bfb6c79d573009e622d64e27
Jose Fletes <joseangelfletes@gmail.com>
2022-08-21 11:47:08 -0700

    commit tree c896868f4660cba49f56d468ebb284baf4065b2a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require "pry"
+array = ["fiddleheads", "okra", "kohlrabi"]
 
+def oxford_comma(array)
+  # array.insert(-2, "and").join(", ")
+  if array.size >= 3
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  else
+    array.join(" and ")
+  end
 end
+
+# binding.pry
